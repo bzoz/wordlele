0b7be572df712828ab053f02bcee6da27fa45477
Bartosz Sosnowski <bartosz@janeasystems.com>
2022-01-15 21:59:51 +0100

    Use spaces instead of tabs, reduce indent to 2 spaces

--- wordele/guesses.h
+++ wordele/guesses.h
@@ -7,72 +7,72 @@
  *  Store a signle guess and its result.
  */
 struct Guess {
-	char word[5] = {0,0,0,0,0};
-	std::uint16_t result = 0;
-	
-	/* Sets result of a guess at position.
-	 *  'm' - green square
-	 *  'i' - yellow square
-	 *  'n' - gray square
-	 */
-	void set_result(int position, char guess_result) {
-		switch (guess_result) {
-		case 'm': result |= 0b11 << (position * 2); break;
-		case 'i': result |= 0b10 << (position * 2); break;
-		case 'n': result |= 0b01 << (position * 2); break;
-		}
-	}
+  char word[5] = {0,0,0,0,0};
+  std::uint16_t result = 0;
+  
+  /* Sets result of a guess at position.
+   *  'm' - green square
+   *  'i' - yellow square
+   *  'n' - gray square
+   */
+  void set_result(int position, char guess_result) {
+    switch (guess_result) {
+    case 'm': result |= 0b11 << (position * 2); break;
+    case 'i': result |= 0b10 << (position * 2); break;
+    case 'n': result |= 0b01 << (position * 2); break;
+    }
+  }
 
-	/* Takes two strings : tested word, e.g. "abbot" and result "minnng"
-	 *  'm' - green square
-	 *  'i' - yellow square
-	 *  'n' - gray square
-	 */
-	static Guess from_result(const char* guess, const char* results) {
-		Guess r;
-		for (int i = 0; i < 5; ++i) {
-			r.word[i] = guess[i];
-			r.set_result(i, results[i]);
-		}
-		return r;
-	}
+  /* Takes two strings : tested word, e.g. "abbot" and result "minnng"
+   *  'm' - green square
+   *  'i' - yellow square
+   *  'n' - gray square
+   */
+  static Guess from_result(const char* guess, const char* results) {
+    Guess r;
+    for (int i = 0; i < 5; ++i) {
+      r.word[i] = guess[i];
+      r.set_result(i, results[i]);
+    }
+    return r;
+  }
 
-	// Creates an instace from a result of testing a give guess with give solution
-	static Guess from_words(const char* guess, const char* solution) {
-		Guess r;
-		std::int8_t counts[256] = {};
-		for (int i = 0; i < 5; ++i) {
-			++counts[solution[i]];
-		}
-		for (int i = 0; i < 5; ++i) {
-			r.word[i] = guess[i];
-			if (guess[i] == solution[i]) {
-				--counts[guess[i]];
-				r.set_result(i, 'm');
-			}
-			else if (counts[guess[i]] > 0) {
-				--counts[guess[i]];
-				r.set_result(i, 'i');
-			}
-			else {
-				r.set_result(i, 'n');
-			}
-		}
-		return r;
-	}
+  // Creates an instace from a result of testing a give guess with give solution
+  static Guess from_words(const char* guess, const char* solution) {
+    Guess r;
+    std::int8_t counts[256] = {};
+    for (int i = 0; i < 5; ++i) {
+      ++counts[solution[i]];
+    }
+    for (int i = 0; i < 5; ++i) {
+      r.word[i] = guess[i];
+      if (guess[i] == solution[i]) {
+        --counts[guess[i]];
+        r.set_result(i, 'm');
+      }
+      else if (counts[guess[i]] > 0) {
+        --counts[guess[i]];
+        r.set_result(i, 'i');
+      }
+      else {
+        r.set_result(i, 'n');
+      }
+    }
+    return r;
+  }
 
-	double score() const {
-		double score = 0;
-		for (int p = 0; p < 5; ++p) {
-			switch ((result >> (p * 2)) & 0b11) {
-			case 0b00: break;
-			case 0b01: break;
-			case 0b10: score += 0.5; break;
-			case 0b11: score += 1; break;
-			}
-		}
-		return score;
-	}
+  double score() const {
+    double score = 0;
+    for (int p = 0; p < 5; ++p) {
+      switch ((result >> (p * 2)) & 0b11) {
+      case 0b00: break;
+      case 0b01: break;
+      case 0b10: score += 0.5; break;
+      case 0b11: score += 1; break;
+      }
+    }
+    return score;
+  }
 };
 
 /*
@@ -80,25 +80,25 @@ struct Guess {
 */
 class Guesses {
 public:
-	// Add new guess
-	void add_guess(Guess guess) {
-		guesses.push_back(guess);
-	}
-	/* Test if tried_solution will produce the same guess results as previous guesses
-	 *  - proper solution should always return true here.
-	 */
-	bool matches(const char* tried_solution) const {
-		for (const auto& guess : guesses) {
-			if (strncmp(tried_solution, guess.word, 5) == 0) {
-				return false;
-			}
-			auto tried_guess = Guess::from_words(guess.word, tried_solution);
-			if (guess.result != tried_guess.result) {
-				return false;
-			}
-		}
-		return true;
-	}
+  // Add new guess
+  void add_guess(Guess guess) {
+    guesses.push_back(guess);
+  }
+  /* Test if tried_solution will produce the same guess results as previous guesses
+   *  - proper solution should always return true here.
+   */
+  bool matches(const char* tried_solution) const {
+    for (const auto& guess : guesses) {
+      if (strncmp(tried_solution, guess.word, 5) == 0) {
+        return false;
+      }
+      auto tried_guess = Guess::from_words(guess.word, tried_solution);
+      if (guess.result != tried_guess.result) {
+        return false;
+      }
+    }
+    return true;
+  }
 private:
-	std::vector<Guess> guesses;
+  std::vector<Guess> guesses;
 };
--- wordele/main.cpp
+++ wordele/main.cpp
@@ -9,52 +9,58 @@
 
 // Finds a word that minimizes the average number of possible solutions
 void minimize_avgerage_possible_solutions(const std::vector<const char*>& all_words, const std::vector<const char*>& possible_solutions) {
-	std::mutex mutex;
-	unsigned cnt = 0;
-	unsigned min_word_cnt = 0xffffffff;
-	const char* min_word = "(none)";
+  std::mutex mutex;
+  unsigned cnt = 0;
+  unsigned min_word_cnt = 0xffffffff;
+  const char* min_word = "(none)";
 
-	concurrency::parallel_for_each(begin(all_words), end(all_words), [&](const char* tested_word) {
-		unsigned int this_cnt = 0;
-		for (auto tested_solution : possible_solutions) {
-			Guesses guess;
-			guess.add_guess(Guess::from_words(tested_word, tested_solution));
-			for (auto word : possible_solutions) {
-				if (guess.matches(word))
-					++this_cnt;
-			}
-		}
-		mutex.lock();
-		++cnt;
-		if (this_cnt < min_word_cnt) {
-			min_word_cnt = this_cnt;
-			min_word = tested_word;
-			std::cout << cnt << "/" << all_words.size() << " " << min_word << " - " << min_word_cnt << "\n";
-		}
-		mutex.unlock();
-	});
+  concurrency::parallel_for_each(begin(all_words), end(all_words), [&](const char* tested_word) {
+    unsigned int this_cnt = 0;
+    for (auto tested_solution : possible_solutions) {
+      Guesses guess;
+      guess.add_guess(Guess::from_words(tested_word, tested_solution));
+      for (auto word : possible_solutions) {
+        if (guess.matches(word))
+          ++this_cnt;
+      }
+    }
+    mutex.lock();
+    ++cnt;
+    if (this_cnt < min_word_cnt) {
+      min_word_cnt = this_cnt;
+      min_word = tested_word;
+      std::cout << cnt << "/" << all_words.size() << " " << min_word << " - " << min_word_cnt << "\n";
+    }
+    mutex.unlock();
+  });
 }
 
 int main() {
-	// Load words into vector for easier manipulation
-	std::vector<const char*> all_words(words, words + sizeof(words) / sizeof(*words));
-	std::vector<const char*> possible_solutions(solutions, solutions + sizeof(solutions) / sizeof(*solutions));
+  // Load words into vector for easier manipulation
+  std::vector<const char*> all_words(words, words + sizeof(words) / sizeof(*words));
+  std::vector<const char*> possible_solutions(solutions, solutions + sizeof(solutions) / sizeof(*solutions));
 
-	// tests...
-	std::unordered_set<const char*> tried_words = {
-		"roate", "hault", "tawny"
-	};
-	Guesses guesses;
-	guesses.add_guess(Guess::from_result("roate", "nniin"));
-	guesses.add_guess(Guess::from_result("hault", "nmnni"));
-	guesses.add_guess(Guess::from_result("tawny", "mmnim"));
-	all_words.erase(std::remove_if(begin(all_words), end(all_words), [&](const char* word) {
-		return (tried_words.find(word) != tried_words.end()) || !guesses.matches(word);
-		}), end(all_words));
-	possible_solutions.erase(std::remove_if(begin(possible_solutions), end(possible_solutions), [&](const char* word) {
-		return (tried_words.find(word) != tried_words.end()) || !guesses.matches(word);
-		}), end(possible_solutions));
-	std::cout << possible_solutions.size() << "\n";
-	minimize_avgerage_possible_solutions(possible_solutions, possible_solutions);
+  // tests...
+  std::unordered_set<const char*> tried_words = {
+    "roate", "hault", "tawny"
+  };
+  Guesses guesses;
+  guesses.add_guess(Guess::from_result("roate", "nninn"));
+  guesses.add_guess(Guess::from_result("lysin", "nnnmi"));
+  //guesses.add_guess(Guess::from_result("nasal", "imnnn"));
+  //guesses.add_guess(Guess::from_result("candy", "immnn"));
+  /*all_words.erase(std::remove_if(begin(all_words), end(all_words), [&](const char* word) {
+    return !guesses.matches(word);
+    }), end(all_words));*/
+  possible_solutions.erase(std::remove_if(begin(possible_solutions), end(possible_solutions), [&](const char* word) {
+    return !guesses.matches(word);
+    }), end(possible_solutions));
+  std::cout << possible_solutions.size() << "\n";
+  if (possible_solutions.size() < 10) {
+    for (auto possible_solution : possible_solutions) {
+      std::cout << "  " << possible_solution << "\n";
+    }
+  }
+  minimize_avgerage_possible_solutions(possible_solutions, possible_solutions);
 
 }
